4aad9c97ce2cb323f64d1812a8da2d45836301a1
Ravi Narasimha <92355929+ravinarasimhapogaku@users.noreply.github.com>
2024-04-02 15:55:29 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,5 @@
+#command prompt 
+cd C:\Program Files\Microsoft Office\Office16
+C:\Program Files\Microsoft Office\Office16\cscript ospp.vsd /sethst:kms.03k.org
+  \Program Files\Microsoft Office\Office16\cscript ospp.vbs /act
+  # please be reminded that you have to run the commandprompt in adminstrator mode only else you will get error or key will not be activated
